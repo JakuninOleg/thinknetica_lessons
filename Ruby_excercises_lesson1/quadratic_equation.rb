e79639a409b09baf7c7626a4f3cb38a8651415cf
Oleg Jakunin <oleg.kemz@gmail.com>
2018-01-23 18:23:49 +0500

    Рефакторинг и исправление замечаний

--- Ruby_excercises_lesson1/quadratic_equation.rb
+++ Ruby_excercises_lesson1/quadratic_equation.rb
@@ -11,18 +11,18 @@ b = gets.chomp.to_f
 puts 'Введите третий коэффициент c:'
 c = gets.chomp.to_f
 
-d = (b**2) - (4*a*c)
+d = (b**2) - (4 * a * c)
 
 puts "Дискриминант: #{d}"
 
 if d < 0
   puts 'Квадратных корней нет'
 elsif d == 0
-  z = (-b) / (2*a)
+  z = (-b) / (2 * a)
   puts "Корень уравнения: #{z}"
 else
   root = Math.sqrt(d)
-  x = ((-b) + root) / (2*a)
-  y = ((-b) - root) / (2*a)
+  x = ((-b) + root) / (2 * a)
+  y = ((-b) - root) / (2 * a)
   puts "Корень уравнения x1: #{x}, корень уравнения x2: #{y}"
 end
