0354647025e511a64a7f74687cdbe618884941e0
Oleg Jakunin <oleg.kemz@gmail.com>
2018-02-04 17:10:49 +0500

    instance_counter module improved

--- Ruby_excercises_lesson5/instance_counter.rb
+++ Ruby_excercises_lesson5/instance_counter.rb
@@ -7,16 +7,13 @@ module InstanceCounter
 
     module ClassMethods
       attr_accessor :instances
-
-      def instances
-        @instances ||= 0
-      end
     end
 
     module InstanceMethods
       private
 
       def register_instance
+        self.class.instances ||= 0
         self.class.instances += 1
       end
     end
